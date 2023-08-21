13e08ea5d3b82373f5eb87ec1367bd8527dffd12
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-21 17:26:17 -0400

    edited index + colorkeywords to properly fs, updated questions

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // Require external files needed to run index.js
 const fs = require("fs")
 const inquirer = require('requirer')
+// const colorKeywords = require('./lib/colorKeywords')
 const questions = require('./lib/questions')
 const setShape = require(`./lib/setShape`)
 let SVG = require("./examples/logo.svg")
+const { colorKeywords } = require("./lib/colorKeywords")
 
 let example = new SVG()
+
+// function to create new svg file using inquirer response and file system
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(SVG, svg, ()=> console.log('Generated logo.svg'));
+}
+colorKeywords
+
+// initialize, ask questions then createLogo using responses, catch any errors
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
